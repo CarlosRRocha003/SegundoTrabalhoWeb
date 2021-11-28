0b0e883c7badaebb0e62de3afa3e343a5625407e
Carlos Ribeiro <carlos.rib.rocha@gmail.com>
2021-11-28 20:38:01 -0300

    Debug false

--- TrabalhoWeb/settings.py
+++ TrabalhoWeb/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b38n$bf#52+t=s)%z5&0*x9c*ox08oso#1k&=k=b%f@b504++5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
